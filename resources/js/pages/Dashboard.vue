cfd806d555c84a5fa3e90aeed58aa272f189fc16
Mian Muhammad Imran Shah <shahmian@gmail.com>
2025-07-07 21:34:47 -0500

    First Deployment.

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -19,7 +19,7 @@ const breadcrumbs: BreadcrumbItem[] = [
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <h1>Hurrayy..</h1>
+                    <h1>Success</h1>
                 </div>
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
